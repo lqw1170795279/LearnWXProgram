9b77dc6ab3870306c56bae15c8b8ac4c7cd8d7a0
Laiqinwei <1214479643@qq.com>
2019-10-29 17:21:17 +0800

    展示弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,57 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  showToast(){
+    wx.showToast({
+      title: '加载中...',
+      duration: 3000,
+      icon: "loading",
+      mask: true,
+      success: function(res){
+        console.log(res)
+      },
+      fail: function(err){
+        console.log('2')
+      },
+      complete: function(){
+        console.log('3')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  showModal(){
+    wx.showModal({
+      title: 'showModal',
+      content: 'showModal-content',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      confirmColor: 'green',
+      success(res){
+        if(res.confirm){
+          console.log('点击确定')
+        }else if(res.cancel){
+          console.log('点击取消')
+        }
+      }
+    })
   },
+  showLoading(){
+    wx.showLoading({
+      title: 'showLoading',
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    setTimeout(function(){
+      wx.hideLoading()
+    },2000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showActionSheet(){
+    wx.showActionSheet({
+      itemList: ['1','2','3'],
+      success(res){
+        console.log(res.tapIndex)
+      },
+      fail(res){
+        console.log(res.errMsg)
+      }
+    })
   }
 })
