b4b6348de6e6e1e7587d8f5a84bc906025e939ff
Laiqinwei <1214479643@qq.com>
2019-11-01 18:56:39 +0800

    未完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,38 @@
 // pages/home/home.js
+import{
+  getMultiData
+}from '../../service/home.js'
+
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    banners: [],
+    recommends: [],
+    titles: ['流行', '新款', '精选' ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //1.请求轮播图及推荐数据
+    getMultiData().then(res=>{
+    //2.取出轮播图和推荐数据
+    //console.log(res)
+    const banners = res.data.data.banner.list;
+    const recommends =res.data.data.recommend.list;
+    //3.将banners和recommends发到data
+    this.setData({
+      banners: banners,
+      recommends: recommends
+    })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTabClick(event){
+    //console.log(event)
+    //取出index
+    const index=event.detail.index;
+    console.log(index);
   }
 })
